8e4619707be09fbdf16b2386e02ddcad74ba69a4
Zubaidah <zb64@njit.edu>
2024-02-02 11:14:20 -0500

    course description style

---
+++ footer.php
@@ -0,0 +1,12 @@
+<!-- embedded/internal stlye -->
+<style>
+    h5 {
+        color: #228b22;
+        background-color:#696969;
+    }
+</style>
+<footer>
+    <h5>
+        <p>Zubaidah Bandele, Feb 2, 2024, IT202-006, zb64@njit.edu</p>
+    </h5>
+</footer>
---
+++ header.php
@@ -0,0 +1,5 @@
+<header>
+    <img src="html_image (1).png" alt="html image" width="58">
+    <h1 style="color: #228b22; background-color: #696969;">Course Name: Advanced Internet Applications</h1>
+    <h2>Course Subject: IT-302</h2>
+</header>
